e95977467f84ab755d92a761bd7706adda491595
nectic <seydou.doumbia@gmail.com>
2019-04-10 19:20:24 +0200

    commit 2

--- src/trec/org/terrier/applications/batchquerying/TRECQuerying.java
+++ src/trec/org/terrier/applications/batchquerying/TRECQuerying.java
@@ -777,8 +777,8 @@ public class TRECQuerying {
 		// iterating through the queries
 		while (querySource.hasNext()) {
 			String query = querySource.next();
-			
 			String qid = querySource.getQueryId();
+			qid = qid.substring(1,qid.length());
 			// process the query
 			long processingStart = System.currentTimeMillis();
 			processQueryAndWrite(qid, query, c, c_set);
